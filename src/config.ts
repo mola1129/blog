cbfc2403b6342c49e924e6eec9542a10d6805ef2
mola1129 <ms.mola1129@gmail.com>
2023-03-26 03:11:27 +0900

    update settings for Pacan

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://mola1129.dev",
+  author: "mola1129",
+  desc: "Pacan: mola1129's tech blog",
+  title: "Pacan",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
